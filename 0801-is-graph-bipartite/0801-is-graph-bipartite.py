2ffdd496c3d8742bcc30f58b2aeda60573f4ae25
Tushar_Solan <Solantushar@gmail.com>
2026-06-19 18:53:03 +0530

    0801-IS-GRAPH-BIPARTITE Stats: Time: 51 ms (5.51%), Space: 20.7 MB (23.48%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,31 +1,53 @@
 from collections import deque
 class Solution:
-    def isBipartite(self, graph: List[List[int]]) -> bool:
+     def isBipartite(self, graph: List[List[int]]) -> bool:
 
-        
-        def bfs(snode):
-            q=deque()
-            q.append(snode)
-            color[snode] = 0 
-            while q:
-                node=q.popleft()
-                 
-                for nei in graph[node]:
-                    if color[nei] == -1 :
-                        color[nei] = 1 -color[node]
-                        q.append(nei)
-                    elif color[node] == color[nei] :
-                        return False  
-            return True              
+        def dfs(snode, col):
+            color[snode] = col
 
+            for nei in graph[snode]:
+                if color[nei] == -1:
+                    if dfs(nei, 1 - col) == False:
+                        return False
 
+                elif col == color[nei]:
+                    return False
 
+            return True
         n=len(graph)
-        color=[-1] * n
+        color = [-1] * n
+
         for i in range(n):
-            if color[i] == -1 :
-                if bfs(i) == False :
+            if color[i] == -1:
+                if dfs(i, 0) == False:
                     return False
         return True            
+    # def isBipartite(self, graph: List[List[int]]) -> bool:
+
+        
+    #     def bfs(snode):
+    #         q=deque()
+    #         q.append(snode)
+    #         color[snode] = 0 
+    #         while q:
+    #             node=q.popleft()
+                 
+    #             for nei in graph[node]:
+    #                 if color[nei] == -1 :
+    #                     color[nei] = 1 -color[node]
+    #                     q.append(nei)
+    #                 elif color[node] == color[nei] :
+    #                     return False  
+    #         return True              
+
+
+
+    #     n=len(graph)
+    #     color=[-1] * n
+    #     for i in range(n):
+    #         if color[i] == -1 :
+    #             if bfs(i) == False :
+    #                 return False
+    #     return True            
 
         
