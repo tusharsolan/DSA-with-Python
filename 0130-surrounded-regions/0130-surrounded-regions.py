1f355b460ee20e1ca17528eaa03d8a1a9c4d18b0
Tushar_Solan <Solantushar@gmail.com>
2026-06-19 14:43:34 +0530

    0130-SURROUNDED-REGIONS Stats: Time: 3 ms (84.7%), Space: 22.7 MB (21.4%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -16,23 +16,25 @@ class Solution:
                 if 0<=nr<n and  0<=nc<m and visited[nr][nc] ==0 and  board[nr][nc] == 'O':
                     dfs(nr,nc)
 
-        #row
-        # visted=[0*m for _ in range(n)]
+        # traverse first and last row
+        #as we want to trav 1st row so i have to trav in col thats why j in range(m)
         for j in range(m):
             if visited[0][j] == 0 and board[0][j] == 'O':
                 dfs(0,j)
             if visited[n-1][j] == 0 and board[n-1][j] == 'O':
                 dfs(n-1,j)
                 
-        #col
+        # traverse first and last column
+        #as we want to trav 1st col so i have to stay or trav in row thats why i in range(n)
         for i in range(n):
             if visited[i][0] == 0  and board[i][0] == 'O':
                 dfs(i,0)
             if visited[i][m-1] == 0 and board[i][m-1] == 'O':
                 dfs(i,m-1)    
-
+        #flip all unvisited 'O' to 'X'
         for i in range(n):
             for j in range(m):
+                    #convert enclosed 'O' to 'X'
                 if visited[i][j] == 0 and board[i][j] == 'O':
                     board[i][j] = 'X'
         return board            
